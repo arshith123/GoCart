2bffa61a44e2f205c28cacf05757cce6435b23e3
Arshith <arshith386@gmail.com>
2025-11-19 19:29:46 +0530

    Revert "installed and configured shadcn package"
    This reverts commit eacd60a693ac6ae472d7d149355f99471de295d6.

--- src/pages/admin/products/ProductList.jsx
+++ src/pages/admin/products/ProductList.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import Container from '../../../components/common/Container'
 import { ListFilter, Plus } from 'lucide-react'
 import Heading from '../../../components/common/Heading'
-import { useNavigate } from 'react-router-dom'
 
 const ProductList = () => {
     const [searchInput, setsearchInput] = useState("");
@@ -47,8 +46,7 @@ const ProductList = () => {
             stock: 0,
             status: "Inactive",
         },
-    ]);
-    const navigate = useNavigate();
+    ])
     return (
         <Container>
             <Heading value="Manage Products" />
@@ -65,7 +63,7 @@ const ProductList = () => {
                     <button className='flex gap-2 items-center border border-gray-400 py-2 px-4 rounded-4xl'>
                         <span><ListFilter /></span> <span className='font-sans font-semibold text-sm'> Sort by</span>
                     </button>
-                    <button className='flex gap-2 items-center border border-gray-400 py-2 px-4 rounded-4xl bg-black' onClick={()=>navigate("/admin/product-add")}>
+                    <button className='flex gap-2 items-center border border-gray-400 py-2 px-4 rounded-4xl bg-black'>
                         <span><Plus color='white' /></span> <span className='font-sans font-semibold text-sm text-white'> Add Product</span>
                     </button>
                 </div>
